0d41b5cd4587429ecd873a9f7351b06e19ffba7b
Andreabafa1444 <al331696@edu.uaa.mx>
2025-09-10 22:55:27 -0600

    parcial01 - add CORS policy

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,29 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit{
-
+export class App implements OnInit {
   private http = inject(HttpClient);
   protected readonly title = signal('Dating App');
-    ngOnInit(): void {
-    this.http.get("https://localhost:5001/api/members").subscribe({
-      next: (response) => {
-        console.log(response);
-      },
-      error: (error) => {
-        console.error(error);
-      },
-      complete: () => {
-        console.log("Request completed");
-      }
-    });
+  protected members = signal<any>([]);
+
+  async ngOnInit(): Promise<void> {
+    this.members.set(await this.getMembers());
   }
+async getMembers(): Promise<Object> {
+  try {
+    const result = await lastValueFrom(this.http.get('http://localhost:5001/api/members'));
+    console.log("Miembros recibidos:", result); // 👈 Aquí el log
+    return result;
+  } catch (error) {
+    console.log("Error al obtener miembros:", error);
+    throw error;
+  }
+}
+
 }
