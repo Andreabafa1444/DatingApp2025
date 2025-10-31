63e6bd725f73db80c13f6b6a532eca53d99049e0
Andreabafa1444 <116229674+Andreabafa1444@users.noreply.github.com>
2025-10-30 23:07:36 -0600

    Parcial02 - Add routes with view transitions

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,28 +1,30 @@
 import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { lastValueFrom } from 'rxjs';
 import { InitService } from '../core/services/init-service';
+import { lastValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
-      provideHttpClient(), //esta listo para la inyeccion
+    provideRouter(routes, withViewTransitions()),
+    provideHttpClient(),
     provideAppInitializer(async () => {
       const initService = inject(InitService);
 
       return new Promise<void>((resolve) => {
-        setTimeout(async() => {
-          try{
-            return lastValueFrom(initService.init());
+        setTimeout(async () => {
+          try {
+            return lastValueFrom(initService.init())
           } finally {
-            const splash = document.getElementById('initial-splash');
+            const splash = document.getElementById("initial-splash");
+
             if (splash) {
               splash.remove();
             }
+
             resolve();
           }
         }, 500);
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 import { NgClass } from '@angular/common';
 
 
@@ -12,37 +8,9 @@ import { NgClass } from '@angular/common';
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Nav, RouterOutlet,NgClass],
+  imports: [Nav, RouterOutlet],
 })
-export class App implements OnInit {
-private accountService = inject(AccountService);
-  protected router= inject(Router)
-  private http: HttpClient = inject(HttpClient);
-  protected readonly title = signal('Dating App');
-  protected members = signal<User[]>([]);
+export class App {
+  protected router = inject(Router);
 
-  async ngOnInit(): Promise<void> {
-        this.setCurrentUser();
-    this.members.set(await this.getMembers());
-  }
-setCurrentUser(){
-    const userString = localStorage.getItem("user");
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-   async getMembers(): Promise<User[]> {
-    try {
-      const result = await lastValueFrom(
-        this.http.get('http://localhost:5001/api/members')
-      );
-      console.log("Miembros recibidos:", result);
-     return lastValueFrom (this.http.get<User[]>("https://localhost:7131/api/members"))
-
-    } catch (error) {
-      console.log("Error al obtener miembros:", error);
-      throw error;
-    }
-  }
 }
