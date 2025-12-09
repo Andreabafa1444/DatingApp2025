77afde057e815220ec5aafce04cb4d34019ec760
Andreabafa1444 <116229674+Andreabafa1444@users.noreply.github.com>
2025-12-08 23:01:09 -0600

    parcial03 - Add Member interface to SPA

--- API/Extensions/AppUserMapper.cs
+++ API/Extensions/AppUserMapper.cs
@@ -4,14 +4,14 @@ using API.Interfaces;
 
 namespace API.Extensions;
 
-public static class AppUserExtensions
-{
+public static class AppUserMapper{
     public static UserResponse ToDto(this AppUser user, ITokenService tokenService)
     {
         return new UserResponse
         {
             Id = user.Id,
             Email = user.Email,
+                        ImageUrl = user.ImageUrl,
             DisplayName = user.DisplayName,
             Token = tokenService.CreateToken(user)
         };
