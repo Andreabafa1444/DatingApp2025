25a2789422eb409c699a204a07cd15090edd1390
Andreabafa1444 <116229674+Andreabafa1444@users.noreply.github.com>
2025-12-08 21:53:04 -0600

    Backup de carpeta client

---
+++ client/.angular/cache/20.2.2/client/vite/deps/@angular_core_primitives_di.js
@@ -0,0 +1,21 @@
+import {
+  NOT_FOUND,
+  NotFoundError,
+  defineInjectable,
+  getCurrentInjector,
+  inject,
+  isNotFound,
+  registerInjectable,
+  setCurrentInjector
+} from "./chunk-5CC4ERJ4.js";
+import "./chunk-GOMI4DH3.js";
+export {
+  NOT_FOUND,
+  NotFoundError,
+  defineInjectable,
+  getCurrentInjector,
+  inject,
+  isNotFound,
+  registerInjectable,
+  setCurrentInjector
+};
